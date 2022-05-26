22da0efeb8ff2c0c2c4b25681affe0a0c5881a15
Megan Campbell <megancampbell201@gmail.com>
2022-05-26 11:16:19 -0400

    took out dashboard icons we don't need

--- client/fitness/src/components/Logo/index.js
+++ client/fitness/src/components/Logo/index.js
@@ -10,14 +10,6 @@ function Logo() {
         <img className="logo-compact" src="./images/logo-text.png" alt="" />
         <img className="brand-title" src="./images/logo-text.png" alt="" />
       </a>
-
-      <div className="nav-control">
-        <div className="hamburger">
-          <span className="line"></span>
-          <span className="line"></span>
-          <span className="line"></span>
-        </div>
-      </div>
     </div>
   );
 }
