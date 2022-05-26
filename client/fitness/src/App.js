202b123a1eb1d83791bb302bc8e22a4fe4ecece1
Megan Campbell <megancampbell201@gmail.com>
2022-05-26 12:58:05 -0400

    installed react router

--- client/fitness/src/App.js
+++ client/fitness/src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { ReactDOM } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Dashboard from './components/Dashboard.js';
 import Logo from './components/Logo';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+
 
 function App() {
   return (
