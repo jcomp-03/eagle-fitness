3f7b4ab8ea50b21d11760b631245a4fc3571d551
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-05-29 12:30:03 -0400

    Succesfully returning/rendering chart component to page. Saving.

--- client/fitness/src/components/WorkoutStatistics/index.js
+++ client/fitness/src/components/WorkoutStatistics/index.js
@@ -1,26 +1,41 @@
 import React from "react";
-import ApexCharts from "apexcharts";
+// import ApexCharts from "apexcharts";
+import Chart from 'react-apexcharts';
 
 function WorkoutStatistics() {
-  var options = {
-    chart: {
-      type: "line",
+  // data values to plot and name
+  const series = [
+    {
+      name: "sales",
+      data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
     },
-    series: [
-      {
-        name: "sales",
-        data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
-      },
-    ],
+  ];
+
+  // chart options you want to specify
+  const options = {
+    chart: {
+      background: '#f4f4f4',
+      foreColor: '#333'
+    },
     xaxis: {
       categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
     },
   };
 
-  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  // var chart = new ApexCharts(document.querySelector("#chart"), options);
 
-    return <h1>This is h1 text</h1>;
-  
+  return (
+    <div>
+      <h1>This is h1 text</h1>
+      <Chart 
+        options={options}
+        series={series}
+        type="line"
+        height="350"
+        width="100%"
+        />
+    </div>
+  );
 }
 
 export default WorkoutStatistics;
---
+++ client/fitness/src/components/WorkoutStatisticsLineChart/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Chart from 'react-apexcharts';
+
+function WorkoutStatisticsLineChart() {
+    return (
+        <Chart 
+        options={}
+        series={}
+        type="line"
+        height="450"
+        width="100%"
+        />
+    );
+}
+
+export default WorkoutStatisticsLineChart;
