82fb444c4650a26e15a266f77b217682446ed2da
Alex Noble-James <alexnoble143.an@gmail.com>
2022-06-02 13:41:15 -0400

    added react-old to dependencies client

--- server/connection/db.js
+++ server/connection/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/eagle-fitness', {
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
   // useCreateIndex: true,
   // useFindAndModify: false
 });
